66e9ed85c1ed3bb88f776fb373c565a89764ead3
Ashita <achic916@gmail.com>
2026-07-26 00:15:17 +0700

    Bump iOS version to 1.0.8 (169) for Codemagic build

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   "expo": {
     "name": "pinc",
     "slug": "pinc",
-    "version": "1.0.7",
+    "version": "1.0.8",
     "orientation": "portrait",
     "icon": "./assets/icon.png",
     "userInterfaceStyle": "light",
@@ -19,7 +19,7 @@ export default {
       "supportsTablet": false,
       "usesAppleSignIn": true,
       "bundleIdentifier": "com.achic.pinc",
-      "buildNumber": "194",
+      "buildNumber": "169",
       "googleServicesFile": "./GoogleService-Info.plist",
       "config": {
         "googleMapsApiKey": process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY || process.env.GOOGLE_MAPS_API_KEY || "dummy_key_to_prevent_crash"
