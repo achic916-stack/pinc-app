7e4e072d7d14ce340a038922141d299f3ec728c6
Ashita <achic916@gmail.com>
2026-08-02 13:00:03 +0700

    Bump version to 1.1.0 (180) to fix App Store Connect release train closed error

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   "expo": {
     "name": "pinc",
     "slug": "pinc",
-    "version": "1.0.9",
+    "version": "1.1.0",
     "orientation": "portrait",
     "icon": "./assets/icon.png",
     "userInterfaceStyle": "light",
@@ -19,7 +19,7 @@ export default {
       "supportsTablet": false,
       "usesAppleSignIn": true,
       "bundleIdentifier": "com.achic.pinc",
-      "buildNumber": "179",
+      "buildNumber": "180",
       "googleServicesFile": "./GoogleService-Info.plist",
       "config": {
         "googleMapsApiKey": process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY || process.env.GOOGLE_MAPS_API_KEY || "dummy_key_to_prevent_crash"
