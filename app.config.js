417f38925d5f48c6906c4bfdac941aea0f82bfc0
Ashita <achic916@gmail.com>
2026-08-04 19:05:11 +0700

    bump: versionCode 232, buildNumber 208, version 1.1.2

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ export default {
     "name": "pinc",
     "slug": "pinc",
     "scheme": "pinc",
-    "version": "1.1.1",
+    "version": "1.1.2",
     "orientation": "portrait",
     "icon": "./assets/icon.png",
     "userInterfaceStyle": "light",
@@ -20,7 +20,7 @@ export default {
       "supportsTablet": false,
       "usesAppleSignIn": true,
       "bundleIdentifier": "com.achic.pinc",
-      "buildNumber": "207",
+      "buildNumber": "208",
       "googleServicesFile": "./GoogleService-Info.plist",
       "config": {
         "googleMapsApiKey": process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY || process.env.GOOGLE_MAPS_API_KEY || "dummy_key_to_prevent_crash"
@@ -72,7 +72,7 @@ export default {
     },
     "android": {
       "package": "com.achic.pinc",
-      "versionCode": 231,
+      "versionCode": 232,
       "config": {
         "googleMaps": {
           "apiKey": process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY || ""
